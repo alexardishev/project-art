cf28065e655c4a6f9772beea5d89b6860357d2d8
Alex <AAArdyshev@yandex.ru>
2021-09-30 10:33:29 +0300

    sliders

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,10 @@
 import modals from './modules/modals';
-
+import sliders from './modules/slider';
 window.addEventListener('DOMContentLoaded', ()=> {
     'use strict';
 
     modals();
-
+    sliders('.feedback-slider-item', '', '.main-prev-btn', '.main-next-btn');
+    sliders('.main-slider-item', 'vertical');
     
 });
