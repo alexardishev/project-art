81b0296cd7890330ab6d025c6f1a40184b5e7ddf
Alex <AAArdyshev@yandex.ru>
2021-10-13 11:12:13 +0300

    Lazy load

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,9 @@
 import modals from './modules/modals';
 import sliders from './modules/slider';
-import forms from './modules/forms'
+import forms from './modules/forms';
+import mask from './modules/mask';
+import checkTextInputs from './modules/checkTextInputs';
+import showMoreStyles from './modules/showMoreStyles';
 window.addEventListener('DOMContentLoaded', ()=> {
     'use strict';
 
@@ -8,4 +11,9 @@ window.addEventListener('DOMContentLoaded', ()=> {
     sliders('.feedback-slider-item', '', '.main-prev-btn', '.main-next-btn');
     sliders('.main-slider-item', 'vertical');
     forms();
+    mask('[name="phone"]');
+    checkTextInputs('[name="name"]');
+    checkTextInputs('[name="message"]');
+    showMoreStyles('.button-styles', '.styles-2');
+
 });
