6917caada15c9525e7b2e0aea1fbc309ad5ff5f5
Alex <AAArdyshev@yandex.ru>
2021-10-29 10:45:28 +0300

    accordion

--- src/js/main.js
+++ src/js/main.js
@@ -7,6 +7,7 @@ import showMoreStyles from './modules/showMoreStyles';
 import calc from './modules/calc';
 import filter from './modules/filter';
 import pictureSize from './modules/pictureSize';
+import accordion from './modules/accordion';
 
 window.addEventListener('DOMContentLoaded', ()=> {
     'use strict';
@@ -31,5 +32,6 @@ window.addEventListener('DOMContentLoaded', ()=> {
     calc('#size', '#material', '#options', '.promocode', '.calc-price', state);
     filter();
     pictureSize('.sizes-block');
+    accordion('.accordion-heading');
 
 });
